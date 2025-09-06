7f232ce0dd6e8c5d6a6c263659026e4119d5dc50
tayyab011 <129363802+tayyab011@users.noreply.github.com>
2025-09-06 21:20:41 +0600

    did vangachura ui and call api ;3

---
+++ script.js
@@ -0,0 +1,120 @@
+const categoryContainer = document.getElementById("categoryContainer");
+
+//1
+const loadCategories = async()=>{
+    try {
+        const res =await fetch("https://news-api-fs.vercel.app/api/categories");
+        const data = await res.json();
+       showCategories(data.categories);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+loadCategories()
+//3 load news details 
+const loadNewsDetail =async (id) => {
+  try {
+    const res =await fetch(`https://news-api-fs.vercel.app/api/news/${id}`);
+    const data =await res.json();
+    showNewsDetails(data.article);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// 3 show news details on modal
+const showNewsDetails =(data)=>{
+const detailsContainer = document.getElementById("detailsContainer");
+detailsContainer.innerHTML = `
+ <div>
+      <h2 class="text-xs font-bold mb-4">${data.title}</h2>
+      <img class="mb-2"  src=${data.images[3].url} />
+      <p class="mb-2">${data.content} </p>
+    </div>
+`;
+document.getElementById("my_modal_5").showModal();
+}
+
+
+
+
+
+//2
+const loadCardNews =(id)=>{
+    console.log(id)
+    fetch(`https://news-api-fs.vercel.app/api/categories/${id}`)
+      .then((res) => res.json())
+      .then((data) => showCardNews(data.articles));
+
+  /* const actives = document.getElementsByClassName("actives");ei line ta sob active class dhore anbe 
+   console.log("aaaaaaaaa", actives);
+  for (let i of actives) {
+   
+    i.classList.remove("border-b-4", "border-red-400");
+  } 
+
+    const actvBtnStart =  document.getElementById(`act-${id}`);
+    
+    actvBtnStart.classList.add("border-b-4", "border-red-400"); */
+}
+
+//2 show card news
+const showCardNews =(news)=>{
+    console.log(news)
+  const newsContainer =  document.getElementById("newsContainer");
+  newsContainer.innerHTML=""
+ news.map((e)=>{
+   newsContainer.innerHTML += `
+    <div class="card bg-info h-96">
+<img src="${e.image.srcset[8].url}" alt="">
+<p>${e.title}</p>
+<h6>${e.time}</h6>
+<div class="flex justify-between">
+   <button class="btn btn-info">bookmark</button>
+   <button onclick="loadNewsDetail('${e.id}')" class="btn btn-danger">view details</button>
+</div>
+
+   </div>
+   `;
+ })
+
+
+}
+
+
+//1
+const showCategories = (title) => {
+ title.forEach(data => {
+  
+    categoryContainer.innerHTML += `
+     <li id="act-${data.id}" onclick="loadCardNews('${data.id}')"   class="hover:border-b-4 hover:border-red-400 cursor-pointer actives">${data.title}</li>
+    `;
+
+ });
+
+//here the code of event deligations
+
+ //active ancor tag func
+/*  categoryContainer.addEventListener("click",((e)=>{  */
+
+/*  const actives = document.querySelectorAll("li");
+ actives.forEach((e) => {
+    console.log("aaaaaaaaa",e)
+   e.classList.remove("border-b-4", "border-red-400");
+ });  */ //eivabeo kora jay
+
+  
+/*   const actives = document.getElementsByClassName("actives"); */
+
+/* console.log(Array.from(actives)); */ /* array te convert kroechi */
+
+/* if (e.target.localName === "li") {
+ 
+     e.target.classList.add("border-b-4", "border-red-400");
+}
+}))  */
+
+};
+
+
